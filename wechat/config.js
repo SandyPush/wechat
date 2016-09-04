1dd10dd380df75341666231c0213ec0124fd075d
huishuai <broce.hui@gmail.com>
2016-09-04 23:11:52 +0800

    fix

--- wechat/config.js
+++ wechat/config.js
@@ -5,8 +5,10 @@ var wechat_file = path.join(__dirname,'./config/wechat.txt')
 var wechat_ticket_file = path.join(__dirname,'./config/wechat_ticket.txt')
 var config = {
     wechat:{
-        appId :'wx202962b22e755ab5',
-        appSecret:'d4624c36b6795d1d99dcf0547af5443d',
+        // appId :'wx202962b22e755ab5',
+        // appSecret:'d4624c36b6795d1d99dcf0547af5443d',
+        appId :'wx0028c0af550b52f3',
+        appSecret:'65708fccaaf519270108a02b93500297',
         token:'wechat',
         getAccessToken:function(){
             return util.readFileAsync(wechat_file)
