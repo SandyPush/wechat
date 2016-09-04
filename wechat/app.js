af5e004539b74459fe283ee247371d36dd2d6120
huishuai <broce.hui@gmail.com>
2016-09-04 15:24:32 +0800

    fix

--- wechat/app.js
+++ wechat/app.js
@@ -8,7 +8,13 @@ var config = require("./config")
 var reply = require('./wx/reply')
 
 var app =  new Koa()
-
+app.use(function *(next){
+    if(this.url.indexOf('./movie')> -1) {
+        this.body = '<h1> Hi there</h1>'
+        return next
+    }
+    yield next
+})
 app.use(wechat(config.wechat,reply.replay))
 
 app.listen(3000)
