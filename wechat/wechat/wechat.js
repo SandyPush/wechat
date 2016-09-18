ad3c939ad12ab45fba5a58fb2151263da7147e21
huishuai <broce.hui@gmail.com>
2016-09-18 21:38:14 +0800

    commit

--- wechat/wechat/wechat.js
+++ wechat/wechat/wechat.js
@@ -932,7 +932,7 @@ Wechat.prototype.updateTicket = function (access_token){
     return new Promise(function(resolve,reject) {
         request({url:url,json:true}).then(function(response) {
             console.log(response.body)
-            console.log(response.[1])
+            console.log(response[1])
             var data = response[1]
             var now = (new Date().getTime())
             var expires_in = now+(data.expires_in-20)*1000
