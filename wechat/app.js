638ac53f89df04162203031c96ee7e749f0ddc18
huishuai <broce.hui@gmail.com>
2016-09-18 23:07:28 +0800

    commit

--- wechat/app.js
+++ wechat/app.js
@@ -26,7 +26,7 @@ var tpl = heredoc(function(){/*
         <script>
             wx.config({
                 debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                appId: 'wx5254ea70a7ec30c7', // 必填，公众号的唯一标识
+                appId: 'wx202962b22e755ab5', // 必填，公众号的唯一标识
                 timestamp:'<%= timestamp %>', // 必填，生成签名的时间戳
                 nonceStr: '<%= noncestr %>', // 必填，生成签名的随机串
                 signature: '<%= signature %>',// 必填，签名，见附录1
