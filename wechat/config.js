8519ff20bf067bd58b78c156e5edbf68bf67eb0e
huishuai <broce.hui@gmail.com>
2016-09-18 23:11:11 +0800

    commit

--- wechat/config.js
+++ wechat/config.js
@@ -5,10 +5,10 @@ var wechat_file = path.join(__dirname,'./config/wechat.txt')
 var wechat_ticket_file = path.join(__dirname,'./config/wechat_ticket.txt')
 var config = {
     wechat:{
-        appId :'wx202962b22e755ab5',
-        appSecret:'d4624c36b6795d1d99dcf0547af5443d',
-        // appId :'wx5254ea70a7ec30c7',
-        // appSecret:'985767bd8480d56df2966cc61efc0493',
+        // appId :'wx202962b22e755ab5',
+        // appSecret:'d4624c36b6795d1d99dcf0547af5443d',
+        appId :'wx5254ea70a7ec30c7',
+        appSecret:'985767bd8480d56df2966cc61efc0493',
         token:'wechat',
         getAccessToken:function(){
             return util.readFileAsync(wechat_file)
