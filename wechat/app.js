4f262f1aae27c655fc864402a9cb790b35939aad
huishuai <broce.hui@gmail.com>
2016-10-07 23:00:50 +0800

    a

--- wechat/app.js
+++ wechat/app.js
@@ -27,7 +27,7 @@ var tpl = heredoc(function(){/*
         <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
         <script>
             wx.config({
-                debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                 appId: '<%= appId %>', // 必填，公众号的唯一标识
                 timestamp:'<%= timestamp %>', // 必填，生成签名的时间戳
                 nonceStr: '<%= noncestr %>', // 必填，生成签名的随机串
@@ -45,7 +45,7 @@ var tpl = heredoc(function(){/*
                     "downloadImage",
                     "translateVoice"
                 ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-            });
+            })
             wx.ready(function(){
 
                 wx.checkJsApi({
@@ -53,9 +53,36 @@ var tpl = heredoc(function(){/*
                     success: function(res) {
                         console.log(res)
                     }
-                });
-    
-            });
+                })
+                
+                var isRecording= false
+                $("h1").on('tap',function(){
+                    if(!isRecording){
+                        isRecording = true
+                        wx.startRecord({
+                            cancel: function(){
+                                alert('不能录音了')
+                            }
+                        })
+                        return 
+                    }
+                    isRecording=false
+                    wx.stopRecord({
+                        success: function (res) {
+                            var localId = res.localId
+                            wx.translateVoice({
+                               localId: localId,
+                                isShowProgressTips: 1, 
+                                success: function (res) {
+                                    console.log(res.translateResult)
+                                }
+                            })
+                        }
+                    })
+
+                })
+
+            })
 
         </script>
     </body>
